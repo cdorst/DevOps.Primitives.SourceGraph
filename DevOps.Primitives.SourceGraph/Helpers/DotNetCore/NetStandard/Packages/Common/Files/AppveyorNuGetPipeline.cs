d3bbf748e392ac623fc700d290c48771c704ba03
Christopher Dorst <chris@dorst.email>
2018-03-11 12:48:33 -0500

    Update nuget-tools dependency

--- DevOps.Primitives.SourceGraph/Helpers/DotNetCore/NetStandard/Packages/Common/Files/AppveyorNuGetPipeline.cs
+++ DevOps.Primitives.SourceGraph/Helpers/DotNetCore/NetStandard/Packages/Common/Files/AppveyorNuGetPipeline.cs
@@ -16,7 +16,7 @@ environment:
   LOCAL_NUGET_SOURCE_PATH: {CacheDirectory}
 install:
 - ps: >-
-    $source = ""https://cdorst-dev.azureedge.net/build/nuget-tools_07.zip""
+    $source = ""https://cdorst-dev.azureedge.net/build/nuget-tools_08.zip""
 
     $toolsZip = ""{ToolsDirectory}.zip""
 
