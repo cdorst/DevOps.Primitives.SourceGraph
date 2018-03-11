589880f559d9e4aa48763efd16e405b194844554
Christopher Dorst <chris@dorst.email>
2018-03-10 18:56:03 -0600

    Add README content for "Project.Index" repositories

--- DevOps.Primitives.SourceGraph/Helpers/DotNetCore/Common/Files/ReadmeFile.cs
+++ DevOps.Primitives.SourceGraph/Helpers/DotNetCore/Common/Files/ReadmeFile.cs
@@ -21,12 +21,10 @@ namespace DevOps.Primitives.SourceGraph.Helpers.DotNetCore.Common.Files
             if (nuGetPackageInfo != null)
             {
                 var prefix = nuGetPackageInfo.PackageId.Split('.').First();
-                var prefixLower = prefix.ToLower();
-                var fullName = $"{prefix}.{name}";
-                var dashName = name.Replace('.', '-').ToLower();
+                var fullName = GetFullName(prefix, name);
                 content.AppendLine()
-                    .AppendLine($"[![AppVeyor build status](https://img.shields.io/appveyor/ci/{prefixLower}/{dashName}/master.svg?label=AppVeyor&style={BadgeStyle})](https://ci.appveyor.com/project/{prefixLower}/{dashName})")
-                    .AppendLine($"[![NuGet package status](https://img.shields.io/nuget/v/{prefix}.{name}.svg?label=NuGet&style={BadgeStyle})]({GetNuGetLinkUrl(fullName)})")
+                    .AppendLine(GetAppVeyorBadge(prefix, name))
+                    .AppendLine(GetNuGetBadge(name, prefix, fullName))
                     .AppendLine();
                 content.AppendLine("## Description")
                     .AppendLine().AppendLine(nuGetPackageInfo.Description).AppendLine();
@@ -44,8 +42,35 @@ namespace DevOps.Primitives.SourceGraph.Helpers.DotNetCore.Common.Files
                 content.AppendLine("## Version")
                     .AppendLine().AppendLine(nuGetPackageInfo.Version).AppendLine();
             }
+            else if (name == "Project.Index" && Any(nuGetReferences))
+            {
+                // This is a special case for a "Project.Index" repository where each NuGetReference is another project under the same GitHub account
+                var prefix = nuGetReferences.First().Include.Value.Split('.').First(); // MyProject.MyDomain.MyConcern => MyProject
+                var prefixSubStrIdx = prefix.Length + 1;
+                content.AppendLine("## Repositories").AppendLine()
+                    .AppendLine("Name | Version | Links | Badges")
+                    .AppendLine("---- | ------- | ----- | ------");
+                foreach (var dependency in nuGetReferences.OrderBy(pkg => pkg.Include.Value))
+                {
+                    var repoName = dependency.Include.Value.Substring(prefixSubStrIdx);
+                    content.AppendLine($"{repoName} | {dependency.Version.Value} | {GetLinks(dependency, prefix)} | {GetBadges(prefix, repoName)}");
+                }
+                content.AppendLine();
+            }
             return new RepositoryFile("README.md", content.ToString());
         }
+
+        private static string GetAppVeyorBadge(string prefix, string name)
+        {
+            var dashName = name.Replace('.', '-').ToLower();
+            var prefixLower = prefix.ToLower();
+            return $"[![AppVeyor build status](https://img.shields.io/appveyor/ci/{prefixLower}/{dashName}/master.svg?label=AppVeyor&style={BadgeStyle})](https://ci.appveyor.com/project/{prefixLower}/{dashName})";
+        }
+
+        private static string GetBadges(string prefix, string repoName)
+            => $"{GetAppVeyorBadge(prefix, repoName)} {GetNuGetBadge(prefix, repoName)}";
+
+        private static string GetFullName(string prefix, string name) => $"{prefix}.{name}";
 
         private static string GetLinks(NuGetReference dependency, string prefix)
         {
@@ -57,6 +82,12 @@ namespace DevOps.Primitives.SourceGraph.Helpers.DotNetCore.Common.Files
                 links.Append($", [GitHub](https://github.com/{prefix}/{repo})");
             }
             return links.ToString();
+        }
+
+        private static string GetNuGetBadge(string prefix, string name, string fullName = null)
+        {
+            if (string.IsNullOrEmpty(fullName)) fullName = GetFullName(prefix, name);
+            return $"[![NuGet package status](https://img.shields.io/nuget/v/{prefix}.{name}.svg?label=NuGet&style={BadgeStyle})]({GetNuGetLinkUrl(fullName)})";
         }
 
         private static string GetNuGetLink(string name, bool useUrlAsName = false)
