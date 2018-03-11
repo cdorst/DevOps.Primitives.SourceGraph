657dadf78eaec0e5452674d4ba4ee61be9316d6d
Christopher Dorst <chris@dorst.email>
2018-03-11 10:58:11 -0500

    Increment appveyor nuget-tools version

--- DevOps.Primitives.SourceGraph/Helpers/DotNetCore/NetStandard/Packages/Common/Files/AppveyorNuGetPipeline.cs
+++ DevOps.Primitives.SourceGraph/Helpers/DotNetCore/NetStandard/Packages/Common/Files/AppveyorNuGetPipeline.cs
@@ -16,7 +16,7 @@ environment:
   LOCAL_NUGET_SOURCE_PATH: {CacheDirectory}
 install:
 - ps: >-
-    $source = ""https://cdorst-dev.azureedge.net/build/nuget-tools_03.zip""
+    $source = ""https://cdorst-dev.azureedge.net/build/nuget-tools_04.zip""
 
     $toolsZip = ""{ToolsDirectory}.zip""
 
