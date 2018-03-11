b8e5752fd241e573d21bfc52d2ac2e217f79699e
Christopher Dorst <chris@dorst.email>
2018-03-11 15:14:15 -0500

    Add null-check

--- DevOps.Primitives.SourceGraph/Helpers/Common/Accounts/AccountRepositoryBuilder.cs
+++ DevOps.Primitives.SourceGraph/Helpers/Common/Accounts/AccountRepositoryBuilder.cs
@@ -10,10 +10,11 @@ namespace DevOps.Primitives.SourceGraph.Helpers.Common.Accounts
         public static GitHubAccount WithRepositories(this GitHubAccount account,
             params Func<GitHubAccount, Repository>[] repositories)
             => new GitHubAccount(account.AccountSettings,
-                new RepositoryList(repositories
-                    .Select(repo => repo(account))
+                new RepositoryList((repositories?
+                    .Select(repo => repo(account)) ?? new Repository[] { })
                     .Concat(
                         account.RepositoryList?.GetRecords() ?? new Repository[] { })
+                    .Where(r => r.RepositoryNameDescription != null)
                     .Distinct(RepositoryName)
                     .Order()
                     .Select(repo => new RepositoryListAssociation(repo)).ToList()));
