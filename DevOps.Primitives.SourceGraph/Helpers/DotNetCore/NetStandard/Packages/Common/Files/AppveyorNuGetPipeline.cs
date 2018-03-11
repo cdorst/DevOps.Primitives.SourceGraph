d96aa52de924b72e547e7362346d0f469beb856f
Christopher Dorst <chris@dorst.email>
2018-03-11 11:32:06 -0500

    Update appveyor nuget tools version

--- DevOps.Primitives.SourceGraph/Helpers/DotNetCore/NetStandard/Packages/Common/Files/AppveyorNuGetPipeline.cs
+++ DevOps.Primitives.SourceGraph/Helpers/DotNetCore/NetStandard/Packages/Common/Files/AppveyorNuGetPipeline.cs
@@ -16,7 +16,7 @@ environment:
   LOCAL_NUGET_SOURCE_PATH: {CacheDirectory}
 install:
 - ps: >-
-    $source = ""https://cdorst-dev.azureedge.net/build/nuget-tools_04.zip""
+    $source = ""https://cdorst-dev.azureedge.net/build/nuget-tools_05.zip""
 
     $toolsZip = ""{ToolsDirectory}.zip""
 
