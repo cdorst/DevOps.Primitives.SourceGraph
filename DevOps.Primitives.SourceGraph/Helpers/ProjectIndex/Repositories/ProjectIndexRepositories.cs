fde7df739f14bf04a9bf99deef3aabf3f35f7f64
Christopher Dorst <chris@dorst.email>
2018-04-07 01:30:34 -0500

    Extract ComputeVersionHash method for clarity

--- DevOps.Primitives.SourceGraph/Helpers/ProjectIndex/Repositories/ProjectIndexRepositories.cs
+++ DevOps.Primitives.SourceGraph/Helpers/ProjectIndex/Repositories/ProjectIndexRepositories.cs
@@ -1,21 +1,27 @@
 ﻿using System.Collections.Generic;
-using System.Text;
-using static DevOps.Primitives.SourceGraph.Helpers.Common.Repositories.FileRepositories;
 using static DevOps.Primitives.SourceGraph.Helpers.ProjectIndex.Files.DgmlFile;
 using static DevOps.Primitives.SourceGraph.Helpers.ProjectIndex.Files.ProjectIndexReadme;
+using static System.Data.HashFunction.xxHash.xxHashFactory;
+using static System.String;
+using static System.Text.Encoding;
 
 namespace DevOps.Primitives.SourceGraph.Helpers.ProjectIndex.Repositories
 {
     public static class ProjectIndexRepositories
     {
         public static Repository ProjectIndexRepository(string accountName, string emailAddress, IDictionary<string, IEnumerable<string>> projectDirectory)
-            => Repository(
+            => Common.Repositories.FileRepositories.Repository(
                 "Project.Index",
                 "Metaproject directory for other repositories in this GitHub account",
-                System.Data.HashFunction.xxHash.xxHashFactory.Instance.Create().ComputeHash(Encoding.UTF8.GetBytes(string.Join(string.Empty, projectDirectory.Keys))).AsHexString(),
+                ComputeVersionHash(projectDirectory.Keys),
                 emailAddress,
                 accountName,
                 Dgml(projectDirectory),
                 Readme(projectDirectory.Keys));
+
+        private static string ComputeVersionHash(IEnumerable<string> repositoryNames)
+            => Instance.Create()
+                .ComputeHash(UTF8.GetBytes(Join(Empty, repositoryNames)))
+                .AsHexString();
     }
 }
