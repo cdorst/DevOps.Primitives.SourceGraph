f3ab31128156d4c50f1e18230f978fa2f3d01993
Christopher Dorst <chris@dorst.email>
2018-06-17 02:42:38 -0500

    Add ignored local files to Git repo

---
+++ DevOps.Primitives.SourceGraph/Helpers/DotNetCore/NetStandard/Packages/Common/FileSets/GitHubRepoNameHelper.cs
@@ -0,0 +1,10 @@
+﻿using static System.String;
+
+namespace DevOps.Primitives.SourceGraph.Helpers.DotNetCore.NetStandard.Packages.Common.FileSets
+{
+    internal static class GitHubRepoNameHelper
+    {
+        public static string RepoUrl(in string account, in string name)
+            => Concat("https://github.com/", account, "/", name);
+    }
+}
---
+++ DevOps.Primitives.SourceGraph/Helpers/DotNetCore/NetStandard/Packages/Common/FileSets/NuGetPackageCacheCopyrightSettings.cs
@@ -0,0 +1,39 @@
+﻿using DevOps.Primitives.Strings;
+using ProtoBuf;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DevOps.Primitives.SourceGraph
+{
+    [ProtoContract]
+    [Table("NuGetPackageCacheCopyrightSettings", Schema = nameof(SourceGraph))]
+    public class NuGetPackageCacheCopyrightSettings
+    {
+        public NuGetPackageCacheCopyrightSettings() { }
+        public NuGetPackageCacheCopyrightSettings(in NuGetPackageCacheSettings nuGetPackageCacheSettings, in AsciiStringReference copyright)
+        {
+            NuGetPackageCacheSettings = nuGetPackageCacheSettings;
+            Copyright = copyright;
+        }
+        public NuGetPackageCacheCopyrightSettings(in string copyright, in NuGetPackageCacheSettings nuGetPackageCacheSettings)
+            : this(in nuGetPackageCacheSettings, new AsciiStringReference(in copyright))
+        {
+        }
+        public NuGetPackageCacheCopyrightSettings(in string copyright, in string appveyorAzureStorageSecret, in string packageCacheUrl)
+            : this(in copyright, new NuGetPackageCacheSettings(in appveyorAzureStorageSecret, in packageCacheUrl))
+        {
+        }
+
+        [ProtoMember(1)]
+        public int NuGetPackageCacheCopyrightSettingsId { get; set; }
+
+        [ProtoMember(2)]
+        public AsciiStringReference Copyright { get; set; }
+        [ProtoMember(3)]
+        public int CopyrightId { get; set; }
+
+        [ProtoMember(4)]
+        public NuGetPackageCacheSettings NuGetPackageCacheSettings { get; set; }
+        [ProtoMember(5)]
+        public int NuGetPackageCacheSettingsId { get; set; }
+    }
+}
